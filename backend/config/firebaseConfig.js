13ed3e26f88af3335abba6d42c24c8421a8c6557
williamndongmo88 <williamndongmo88@gmail.com>
2025-11-11 12:37:49 +0100

    Fix: config des params de connexion a firebase

--- backend/config/firebaseConfig.js
+++ backend/config/firebaseConfig.js
@@ -1,8 +1,22 @@
 const admin = require('firebase-admin');
-// const path = require('path');
+const path = require('path');
 
-// const serviceAccount = require(path.join(__dirname, 'serviceAccountKey.json'));
-const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+const serviceAccount = require(path.join(__dirname, 
+  {
+  "type": process.env.FIREBASE_TYPE,
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY,
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  "client_id": process.env.FIREBASE_CLIENT_ID,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40solsolutionpdf.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
+));
+// const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
