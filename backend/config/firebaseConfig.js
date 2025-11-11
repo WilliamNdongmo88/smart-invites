479dac424f54716af3833360e5c5f99b13700953
williamndongmo88 <williamndongmo88@gmail.com>
2025-11-11 12:59:52 +0100

    Fix: config des params de connexion a firebase

--- backend/config/firebaseConfig.js
+++ backend/config/firebaseConfig.js
@@ -1,18 +1,28 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = {
-  "type": process.env.FIREBASE_TYPE,
-  "project_id": process.env.FIREBASE_PROJECT_ID,
-  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-  "private_key": process.env.FIREBASE_PRIVATE_KEY,
-  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-  "client_id": process.env.FIREBASE_CLIENT_ID,
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40solsolutionpdf.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-};
+if (process.env.NODE_ENV === 'test') {
+  // Mock simple pour Jest
+  module.exports = {
+    initializeApp: jest.fn(),
+    credential: {
+      cert: jest.fn()
+    }
+  };
+} else {
+  const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_emai: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40solsolutionpdf.iam.gserviceaccount.com",
+    universe_domain: "googleapis.com"
+  };
+}
 // const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
