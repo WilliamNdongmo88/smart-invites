69ee59a4a22707c239264354e11759d23b7a0d78
williamndongmo88 <williamndongmo88@gmail.com>
2025-11-19 09:20:07 +0100

    fix: valeur par defaut des champs migré

--- backend/migrations/20251119072945_add_event_extra_fields.js
+++ backend/migrations/20251119072945_add_event_extra_fields.js
@@ -1,9 +1,9 @@
 exports.up = function (knex) {
   return knex.schema.alterTable("EVENTS", function (table) {
-    table.string("type", 50).notNullable();
-    table.string("budget", 50).notNullable();
-    table.string("event_name_concerned1", 50).notNullable();
-    table.string("event_name_concerned2", 50).notNullable();
+    table.string("type", 50).defaultTo();
+    table.string("budget", 50).defaultTo();
+    table.string("event_name_concerned1", 50).defaultTo();
+    table.string("event_name_concerned2", 50).defaultTo();
   });
 };
 
