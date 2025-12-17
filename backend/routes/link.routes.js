9e28ee1ba8a0f7117b1dfd42a2ca2f4d8ea01fdf
williamndongmo88 <williamndongmo88@gmail.com>
2025-12-17 15:08:08 +0100

    feat: Ajout des routes l'api pour modifier et supprimer les caractéristique d'un lien

--- backend/routes/link.routes.js
+++ backend/routes/link.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const LinkController = require('../controllers/link.controller');
-const { authenticateToken, requireRole } = require('../middlewares/jwtFilter');
+const { authenticateToken } = require('../middlewares/jwtFilter');
 
 router.post('/add-link', authenticateToken, LinkController.addLink);
 router.get('/get-links', authenticateToken, LinkController.getLinks);
