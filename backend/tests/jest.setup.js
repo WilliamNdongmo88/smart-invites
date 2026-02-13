3abf4490ff286c62ecd626acb40b32d9ca087dbf
williamndongmo88 <williamndongmo88@gmail.com>
2026-02-13 22:42:17 +0100

    feat: automatisation de l'annulation du plan pro un mois après la date de souscribtion

--- backend/tests/jest.setup.js
+++ backend/tests/jest.setup.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config({ path: '.env.test' });
 
 const dbConfig = {
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
 };
 
 const TEST_DB_NAME = process.env.MYSQLDATABASE;
