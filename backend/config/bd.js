1951c17ea5a9a92bb0884ee498c1f293d220fcc3
williamndongmo88 <williamndongmo88@gmail.com>
2026-02-15 11:55:47 +0100

    feat: automatisation de l'annulation du plan pro un mois après la date de souscribtion

--- backend/config/bd.js
+++ backend/config/bd.js
@@ -8,11 +8,11 @@ require('dotenv').config({
 const isTest = process.env.NODE_ENV;
 console.log('isTest:', isTest);
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || 3308,
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'dev-root',
-  database: process.env.MYSQL_DATABASE || 'dev_smart_invite_db',
+  host: isTest == 'production' ? process.env.MYSQLHOST : process.env.MYSQL_HOST || 'localhost',
+  port: isTest == 'production' ? process.env.MYSQLPORT : process.env.MYSQL_PORT || 3308,
+  user: isTest == 'production' ? process.env.MYSQLUSER : process.env.MYSQL_USER|| 'root',
+  password: isTest == 'production' ? process.env.MYSQLPASSWORD : process.env.MYSQL_PASSWORD|| 'dev-root',
+  database: isTest == 'production' ? process.env.MYSQLDATABASE : process.env.MYSQL_DATABASE || 'dev_smart_invite_db',
   waitForConnections: true,
   connectionLimit: 10,
   charset: "utf8mb4",
