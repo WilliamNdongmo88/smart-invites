92a3642684ec8b30477d75596bcf16a6ee45d5e3
williamndongmo88 <williamndongmo88@gmail.com>
2026-03-06 18:34:04 +0100

    feat: Test de deployement

--- backend/app.js
+++ backend/app.js
@@ -45,7 +45,6 @@ if (process.env.NODE_ENV !== 'test') {
   app.use("/api/invitation", apiLimiter);
 }
 
-// Notifications sans rate-limit
 app.use("/api/notification", noRateLimit);
 
 app.use(express.json());
